5ba1a8d246136c2f81e77263db3d9091ffeca3e5
Serhii Delihioz <DSerhiyWeb@gmail.com>
2019-01-19 01:05:00 +0200

    dir L7_functions and script-7.1.js are added

---
+++ L7_functions/README.md
@@ -0,0 +1,8 @@
+# Functions
+script-7.1.js
+--------------
+Implementation of bild-in Array methods in own function:
+- Array.forEach(),
+- Array.map(),
+- Array.filter();
+
---
+++ L7_functions/script-7.1.js
@@ -0,0 +1,42 @@
+function forEach (arr, callback) {
+  for(let i = 0; i < arr.length; i++)
+    callback(arr[i], i, arr);
+}
+
+function map (arr, callback) {
+  const newArr = [];
+  
+  for(let i = 0; i < arr.length; i++)
+    newArr[i] = callback(arr[i], i, arr);
+  
+    return newArr
+}
+
+function filter (arr, callback) {
+  const newArr = [];
+    
+  for(let i = 0, j = 0; i < arr.length; i++) {
+    if(callback(arr[i], i, arr)) {
+      newArr[j] = arr[i];
+      j++;
+    }  
+  }  
+  
+  return newArr
+}
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+forEach(arr, function(item, index) {
+  console.log(index, '=>', item);
+});
+
+console.log(map(arr, function(item) {
+  return item * 2;
+}));
+
+console.log(filter(arr, function(item, index, arr) {
+  return item > 3;
+}));
+
+
